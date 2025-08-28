6d4cf7f4449012378be81739c710d82b53dddaf6
Freja Østerbøg <fros@accuranker.com>
2025-08-28 16:03:02 +0200

    fix bug

--- done-tools/open_multiple_raw_html.py
+++ done-tools/open_multiple_raw_html.py
@@ -7,7 +7,7 @@ PLAYWRIGHT_TYPES = ["playwright", "1337", "leet", "3"]
 
 url_prod = lambda keyword_id: f"https://app.accuranker.com/keywords/ranks/{keyword_id}/raw_html/"
 url_local = lambda keyword_id: f"http://localhost.dk:8000/keywords/ranks/{keyword_id}/raw_html/"
-url_playwright = lambda keyword, device: f"http://localhost:1337/google/scrape?keyword={keyword}&device={device}"
+url_playwright = lambda keyword, device: f"http://localhost:1337/google/scrape_ai_overview?prompt={keyword}&device={device}"
 
 description = f"""
 Tool for opening multiple scrapes.
@@ -47,10 +47,11 @@ if __name__ == "__main__":
         print(PLAYWRIGHT_TEXT)
         while True:
             device = input("Device: ").strip()
-            if not device:
+            if device:
+                break
+            else:
                 print("Invalid device, try again")
                 print(break_text)
-                break
 
     print("Input the ids you want to open")
     keywords = []
