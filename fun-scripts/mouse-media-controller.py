99a78f8cb6762e5f4d255cad6842023a72363cb1
freja <freja@osterbog.me>
2026-02-22 12:55:34 +0100

    less indent

--- fun-scripts/mouse-media-controller.py
+++ fun-scripts/mouse-media-controller.py
@@ -18,25 +18,27 @@ def press_key(key: Key):
 
 def on_click(x, y, button, pressed):
     global start_loc
-    if Button.button10 == button:
-        if pressed:
-            thumb_lock.acquire()
-            start_loc = [x, y]
-        else: # released
-            thumb_lock.release()
-            if not scroll_lock.locked():
-                movement_vector = [start_loc[0] - x, start_loc[1] - y]
-                movement_length = math.hypot(movement_vector[0], movement_vector[1])
-                if movement_length < minimum_movement_for_change:  # To make single clicks easier
-                    press_key(Key.media_play_pause)
-                elif abs(movement_vector[0]) > abs(movement_vector[1]):  # Movement is horizontal
-                    if movement_vector[0] > 0:  # Left
-                        press_key(Key.media_previous)
-                    elif movement_vector[0] < 0:  # Right
-                        press_key(Key.media_next)
-            if scroll_lock.locked():
-                scroll_lock.release()
-            start_loc = None
+    if button != Button.button10:
+        return
+    if pressed:
+        thumb_lock.acquire()
+        start_loc = [x, y]
+        return
+    thumb_lock.release()
+    if not scroll_lock.locked():
+        movement_vector = [start_loc[0] - x, start_loc[1] - y]
+        movement_length = math.hypot(movement_vector[0], movement_vector[1])
+        if movement_length < minimum_movement_for_change:  # To make single clicks easier
+            press_key(Key.media_play_pause)
+        elif abs(movement_vector[0]) > abs(movement_vector[1]):  # Movement is horizontal
+            if movement_vector[0] > 0:  # Left
+                press_key(Key.media_previous)
+            elif movement_vector[0] < 0:  # Right
+                press_key(Key.media_next)
+    if scroll_lock.locked():
+        scroll_lock.release()
+    start_loc = None
+
 
 def on_scroll(x, y, dx, dy):
     if thumb_lock.locked():
